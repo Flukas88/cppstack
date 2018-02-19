e086269eefe003e5fd833d54618bddc37caded0d
Luca Francesca <luca@lucafrancesca.me>
2018-02-19 22:06:01 +0000

    Removed using namespace widespreadingly

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include "stack.h"
 #include "utils.h"
 
-using namespace std;
-
 int main(int argc, char **argv) {
   if (checkArgs(argc, argv)) {
     Stack the_stack;
-    vector<string> tokens = the_stack.split(argv[1], ' ');
+    std::vector<std::string> tokens = the_stack.split(argv[1], ' ');
     try {
-      cout << "Res of " << argv[1] << " is " << parseExp(the_stack, tokens);
+      std::cout << "Res of " << argv[1] << " is "
+                << parseExp(the_stack, tokens);
     } catch (const std::invalid_argument &e) {
-      cerr << "exception: " << e.what() << endl;
+      std::cerr << "exception: " << e.what() << std::endl;
     }
   } else {
-    cerr << "You need to specify an exp to evaluate" << endl;
+    std::cerr << "You need to specify an exp to evaluate" << std::endl;
   }
   return 0;
 }
