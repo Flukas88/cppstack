1f5ac004050901cbb1e765091ff75579c909c328
Luca Francesca <luca@lucafrancesca.me>
2018-02-21 13:53:35 +0000

    Update UnitTest.py
    New approach

--- unittest/UnitTest.py
+++ unittest/UnitTest.py
@@ -9,10 +9,13 @@ class CppCalcTest(unittest.TestCase):
         pass
 
     def test_valid_calc(self): 
-        self.assertIn("6",subprocess.check_output(['./cppstack', '"5 1 2 + 4 x + 3 - 2 /"']))
+        
+        self.assertIn("6",subprocess.Popen(["./cppstack", "5 1 2 + 4 x + 3 - 2 /"], 
+                          stdout=subprocess.PIPE).communicate()[0])
 
     def test_invalid_calc(self):
-        self.assertIn("possible", subprocess.check_output(['./cppstack', '"5 5 /"']))
+        self.assertIn("possible", subprocess.Popen(["./cppstack", "5 5 /"], 
+                          stdout=subprocess.PIPE).communicate()[0])
 
 if __name__ == '__main__':
     unittest.main()
