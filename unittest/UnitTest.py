a530e7e4d7e36be8007ff49ec7bbc904eec82b23
Luca Francesca <luca@lucafrancesca.me>
2018-02-21 13:47:06 +0000

    Update UnitTest.py

--- unittest/UnitTest.py
+++ unittest/UnitTest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-from subprocess import PIPE,Popen
+import subprocess 
 import unittest
 
 
@@ -8,11 +8,11 @@ class CppCalcTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_valid_calc(self):
-        self.assertIn("6", subprocess.Popen('../cppstack  "5 1 2 + 4 x + 3 - 2 /"', stdout=PIPE).stdout.read())
+    def test_valid_calc(self): 
+        self.assertIn("6",subprocess.check_output(['../cppstack', '"5 1 2 + 4 x + 3 - 2 /"']))
 
     def test_invalid_calc(self):
-        self.assertIn("possible", subprocess.Popen('../cppstack  "5 5 /"', stdout=PIPE).stdout.read())
+        self.assertIn("possible", subprocess.check_output(['../cppstack', '"5 5 /"']))
 
 if __name__ == '__main__':
     unittest.main()
