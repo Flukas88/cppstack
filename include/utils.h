2fdde5a0d00a86f9cdd2bf31587f27bb4068a0fa
Luca Francesca <luca_francesca@mcafee.com>
2018-02-21 15:11:49 +0000

    Some fix for another missing header
    Signed-off-by: Luca Francesca <luca_francesca@mcafee.com>

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,7 @@
 #define CPPSTACK_UTILS_H
 
 #include <string>
+#include "../include/stack.h"
 
 bool checkArgs(int argc, char **argv);
 std::string parseExp(Stack exp_stack, std::vector<std::string> tokens);
