cd5562dc1383ec0468060dfd36f1ecbfe63ea383
Luca Francesca <luca@lucafrancesca.me>
2018-02-22 19:13:34 +0000

    Fixed old include path

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,35 +1,37 @@
-#include "../include/utils.h"
+#include "utils.h"
 
-bool checkArgs(int argc, char **argv)  noexcept { return argc < 2 ? false : true; }
+bool checkArgs(int argc, char **argv) noexcept {
+  return argc < 2 ? false : true;
+}
 
 std::string parseExp(Stack exp_stack, std::vector<std::string> tokens) {
-    double op1, op2 = 0.0;
-    for (auto const &token : tokens) {
-        if (exp_stack.is_number(token)) {
-            exp_stack.push(std::stod(token));
+  double op1, op2 = 0.0;
+  for (auto const &token : tokens) {
+    if (exp_stack.is_number(token)) {
+      exp_stack.push(std::stod(token));
+    } else {
+      if (token.find('+') != std::string::npos) {
+        op1 = exp_stack.pop();
+        op2 = exp_stack.pop();
+        exp_stack.push(op1 + op2);
+      } else if (token.find('-') != std::string::npos) {
+        op1 = exp_stack.pop();
+        op2 = exp_stack.pop();
+        exp_stack.push(op2 - op1);
+      } else if (token.find('x') != std::string::npos) {
+        op1 = exp_stack.pop();
+        op2 = exp_stack.pop();
+        exp_stack.push(op1 * op2);
+      } else if (token.find('/') != std::string::npos) {
+        op1 = exp_stack.pop();
+        op2 = exp_stack.pop();
+        if ((op1 == 0.0) || (op2 == 0.0)) {
+          throw std::invalid_argument("Division by 0 is not possible!");
         } else {
-            if (token.find('+') != std::string::npos) {
-                op1 = exp_stack.pop();
-                op2 = exp_stack.pop();
-                exp_stack.push(op1 + op2);
-            } else if (token.find('-') != std::string::npos) {
-                op1 = exp_stack.pop();
-                op2 = exp_stack.pop();
-                exp_stack.push(op2 - op1);
-            } else if (token.find('x') != std::string::npos) {
-                op1 = exp_stack.pop();
-                op2 = exp_stack.pop();
-                exp_stack.push(op1 * op2);
-            } else if (token.find('/') != std::string::npos) {
-                op1 = exp_stack.pop();
-                op2 = exp_stack.pop();
-                if ((op1 == 0.0) || (op2 == 0.0)) {
-                    throw std::invalid_argument("Division by 0 is not possible!");
-                } else {
-                    exp_stack.push((op2 / op1));
-                }
-            }
+          exp_stack.push((op2 / op1));
         }
+      }
     }
-    return std::to_string(exp_stack.pop());
+  }
+  return std::to_string(exp_stack.pop());
 }
