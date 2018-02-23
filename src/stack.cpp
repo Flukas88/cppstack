ffe77dc0460a2c7602b5db5520c47da034d2f433
Luca Francesca <luca@lucafrancesca.me>
2018-02-23 13:11:02 +0000

    Update stack.cpp
    Removed unused std::

--- src/stack.cpp
+++ src/stack.cpp
@@ -23,7 +23,7 @@ const double Stack::pop() {
 
 bool Stack::is_number(const std::string &s) {
     std::string::const_iterator it = s.begin();
-    while (it != s.end() && std::isdigit(*it))
+    while (it != s.end() && isdigit(*it))
         ++it;
     return !s.empty() && it == s.end();
 }
