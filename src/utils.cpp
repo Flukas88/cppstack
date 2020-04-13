e44b69b2660504aa42aa13f1dfb40ad29724f851
Luca Francesca <lfrancesca@salesforce.com>
2020-04-13 12:38:36 +0100

    Fixing bug with division and 0

--- src/utils.cpp
+++ src/utils.cpp
@@ -30,7 +30,7 @@ std::string parseExp(Stack exp_stack, const std::vector<std::string>& tokens) {
       } else if (token.find('/') != std::string::npos) {
         op1 = exp_stack.pop();
         op2 = exp_stack.pop();
-        if ((op1 == 0.0) || (op2 == 0.0)) {
+        if (op1 == 0.0) {
           throw std::invalid_argument("Division by 0 is not possible!");
         } else {
           exp_stack.push((op2 / op1));
