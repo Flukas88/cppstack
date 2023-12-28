6bf270d913b113d38853d5f5fe67730ae633322d
Luca Francesca <luca@lucafrancesca.me>
2023-12-28 14:01:12 +0100

    Using argparse

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
 #include <stack.hpp>
 #include <utils.hpp>
+#include <argparse/argparse.hpp>
 
 
 auto main(int argc, char **argv) -> int {
-  if (checkArgs(argc)) {
+  argparse::ArgumentParser program("cppstack");
+
+  program.add_argument("-e", "--expression")
+    .default_value(std::string("0"))
+    .required()
+    .help("specify the expression to parse.");
+
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::exception& err) {
+    std::cerr << err.what() << '\n';
+    std::cerr << program;
+    return 1;
+  }
+
+  try {
+    auto input = program.get<std::string>("expression");
     Stack theStack;
-    std::vector<std::string> tokens = tokenize(argv[1], ' ');
+    std::vector<std::string> tokens = tokenize(input, ' ');
     try {
-      std::cout << "Res of " << argv[1] << " is " << parseExp(theStack, tokens)
+      std::cout << "Res of " << input << " is " << parseExp(theStack, tokens)
                 << "\n";
     } catch (const std::invalid_argument &e) {
       std::cerr << "exception: " << e.what() << '\n';
     }
-  } else {
-    std::cerr << "You need to specify an exp to evaluate such as '5 10 x 2 /'";
-    std::cerr << '\n';
   }
+  catch (const std::exception& err) {
+          std::cerr << err.what() << '\n';
+          std::cerr << program;
+          return 1;
+  }
+
   return 0;
 }
