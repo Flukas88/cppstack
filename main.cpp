6b264980f5e9d365fe5469c421f095532bec0145
Luca Francesca <luca@lucafrancesca.me>
2024-04-14 18:48:21 +0200

    Added logging

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stack.hpp>
 #include <utils.hpp>
 #include <argparse/argparse.hpp>
+#include <glog/logging.h>
 
 
 auto main(int argc, char **argv) -> int {
   argparse::ArgumentParser program("cppstack", "0.1.1");
+  google::InitGoogleLogging(argv[0]);
 
   program.add_argument("-e", "--expression")
     .default_value(std::string("0"))
@@ -23,17 +25,22 @@ auto main(int argc, char **argv) -> int {
   try {
     auto input = program.get<std::string>("expression");
     const std::vector<std::string> tokens = tokenize(input, ' ');
+
     try {
       const Stack theStack;
-      std::cout << "Res of " << input << " is " << parseExp(theStack, tokens)
+      auto res = parseExp(theStack, tokens);
+      if (res == "nan") {
+        LOG(ERROR) << "Division by 0 is not allowed\n";
+        return 1;
+      }
+      std::cout << "Res of " << input << " is " << res
                 << "\n";
     } catch (const std::invalid_argument &e) {
-      std::cerr << "exception: " << e.what() << '\n';
+      LOG(ERROR) << "exception: " << e.what() << '\n';
     }
   }
   catch (const std::exception& err) {
-          std::cerr << err.what() << '\n';
-          std::cerr << program;
+          LOG(ERROR) << err.what() << '\n' << program << '\n';
           return 1;
   }
 
